d0d4a0f6fcca2dd3967e56665828072f816a46eb
kaustubh <banekaustubh27@gmail.com>
2025-12-04 00:45:33 +0530

    pairs

--- stl/pairs.cpp
+++ stl/pairs.cpp
@@ -4,12 +4,25 @@ using namespace std;
 void initialPairExplanation()
 {
     pair<int, int> p = {1, 3};
-    cout << p.first;
-    cout << p.second;
+    cout << p.first <<" ";
+    cout << p.second<<" ";
+}
+
+
+void pairInPair(){
+    pair<int , pair<int , int>> p = {1,{2,3}};
+    cout<<p.second.first;
+}
+
+void pairsWithArray(){
+    pair<int,int> arr[]={{1,2},{3,4},{5,6},{7,8}};
+    cout<<arr[2].first;
 }
 
 int main()
 {
-    initialPairExplanation();
+    // initialPairExplanation();
+    // pairInPair();
+    pairsWithArray();
     return 0;
 }
