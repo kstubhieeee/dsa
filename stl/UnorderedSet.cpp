2d553916b089725f60252d517150e108b55a1c9f
kaustubh <banekaustubh27@gmail.com>
2025-12-07 17:13:56 +0530

    unordered set

---
+++ stl/UnorderedSet.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    unordered_set<int> st;
+    // lower_bound and upper_bound function
+    // does not works, rest all functions are same
+    // as above, it does not stores in any
+    // particular order it has a better complexity
+    // than set in most cases, except some when collision happens
+    return 0;
+}
