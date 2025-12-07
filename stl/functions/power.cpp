813c32661455028f61b6d8a5fa7cf5eab6bf2df4
kaustubh <banekaustubh27@gmail.com>
2025-12-08 00:49:37 +0530

    power (pow)

---
+++ stl/functions/power.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double base = 2.0, exp = 3.0;
+    double result = pow(base, exp);
+    cout << "2^3 = " << result;
+    return 0;
+}
